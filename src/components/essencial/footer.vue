75016ec1c84abbb30660b83ada9a0fb3fb47663e
jonathas Augusto <jonathas_a.a@hotmail.com>
2021-01-05 23:37:47 -0300

    Atualiza footer - Feita a data do footer ser calculada automaticamente; - Inserido links do github

--- src/components/essencial/footer.vue
+++ src/components/essencial/footer.vue
@@ -3,23 +3,35 @@
     <v-container fluid class="text-center">
       <v-row no-gutters>
         <v-col cols="12" lg="6">
-          <p>Tenha uma lista de mpusica para videokê sempre com você!!!</p>
+          <p>Tenha uma lista de música para videokê sempre com você!!!</p>
         </v-col>
-        <v-col cols="12" lg="2">
-          <h3>contato</h3>
-          <v-icon
-            v-text="icons.mdiWhatsapp"
-            size="50"
-            color="light-green"
-          ></v-icon>
+        <v-col class="text-center" cols="12" lg="2">
+          <div class="d-flex align-center justify-center">
+            <a
+              href="https://api.whatsapp.com/send?phone=5511947466026&text=Gostaria%20de%20alugar%20uma%20m%C3%A1quina%20com%20voc%C3%AA!"
+              target="_blank"
+            >
+              <v-icon
+                v-text="icons.mdiWhatsapp"
+                size="40"
+                color="light-green"
+              ></v-icon>
+              <h4>(11) 9 4746-6026</h4>
+            </a>
+          </div>
         </v-col>
         <v-col cols="12" lg="3">
           Atualizado até o cartucho {{ version }}
         </v-col>
       </v-row>
       <v-row no-gutters>
-        <v-col cols="12" class="d-flex justify-center">
-          © 2021 Copyright: <span> J-Yoharu / joaodiadema</span>
+        <v-col cols="12" class="justify-center">
+          © {{ new Date().getFullYear() }} Copyright
+          <div>
+            <v-icon v-text="icons.mdiGithub"></v-icon>
+            <a href="https://github.com/j-yoharu">J-Yoharu</a> /
+            <a href="https://github.com/japostulo">Japostulo</a>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -27,13 +39,14 @@
 </template>
 
 <script>
-import { mdiWhatsapp } from "@mdi/js";
+import { mdiWhatsapp, mdiGithub } from "@mdi/js";
 
 export default {
   data() {
     return {
       icons: {
-        mdiWhatsapp
+        mdiWhatsapp,
+        mdiGithub
       },
       version: null
     };
