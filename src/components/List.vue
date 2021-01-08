c28c74dc9780561d70133e3c88b9f3708b41c010
jonathas Augusto <jonathas_a.a@hotmail.com>
2021-01-08 01:10:24 -0300

    Faz otimizacao referente a pesquisa; - retirada de chamada de funçoes desnecessarias; - Foi retirado bd duplicado no componente searchbar; - Feita a manipulaçao do bd somente uma vez ao carregar o programa, em vez de a cada pesquisa; - Atribuido um index pre-formatado nas musicas para fazer a pesquisa comparar menos caracteres no autocomplete.

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,32 +1,39 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="songs"
-    :search="search"
-    :loading="loading"
-    disable-pagination
-    :mobile-breakpoint="0"
-    hide-default-footer
-    no-data-text="Pesquise por um cantor ou música"
-  >
-    <template v-slot:top>
-      <SearchBar @search="searchSongs($event)" />
-    </template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="songsFilter"
+      disable-pagination
+      :mobile-breakpoint="0"
+      hide-default-footer
+      no-data-text="Pesquise por um cantor ou música"
+    >
+      <template v-slot:top>
+        <SearchBar
+          @loading="loading = $event"
+          @search="songsFilter = $event"
+          :data="db"
+        />
+      </template>
 
-    <template v-slot:body="{ items }">
-      <tbody>
-        <tr
-          v-for="(music, index) in items"
-          :key="index"
-          :class="index % 2 == 0 ? 'bg-green' : ''"
-        >
-          <td>{{ music.cantor }}</td>
-          <td>{{ music.titulo }}</td>
-          <td>{{ music.cod }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-data-table>
+      <template v-slot:body="{ items }">
+        <tbody>
+          <tr
+            v-for="(music, index) in items"
+            :key="index"
+            :class="index % 2 == 0 ? 'bg-green' : ''"
+          >
+            <td>{{ music.cantor }}</td>
+            <td>{{ music.titulo }}</td>
+            <td>{{ music.cod }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-data-table>
+    <div class="d-flex justify-center mt-10" v-if="loading">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,7 +57,7 @@ export default {
           value: "cod"
         }
       ],
-      songs: [],
+      songsFilter: [],
       search: "",
       selected: "",
       loading: false,
@@ -61,17 +68,33 @@ export default {
     searchSongs(data) {
       this.search = data;
       this.songs = this.db;
+    },
+    textFormat(text) {
+      return text
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .trim();
+    },
+
+    loadSongs() {
+      this.$axios("./bd.json")
+        .then(res => {
+          this.db = res.data.data.map(song => {
+            return {
+              cantor: this.textFormat(song.cantor),
+              titulo: this.textFormat(song.titulo),
+              cod: this.textFormat(song.cod)
+            };
+          });
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     }
   },
   created() {
     this.loading = true;
-    this.$axios("./bd.json")
-      .then(res => {
-        this.db = res.data.data;
-      })
-      .finally(() => {
-        this.loading = false;
-      });
+    this.loadSongs();
   }
 };
 </script>
