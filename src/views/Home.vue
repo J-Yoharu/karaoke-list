6a89defc9503479b97fad0629a149b37d2a8205d
Yoharu <jonathas_a.a@hotmail.com>
2022-06-11 15:21:57 -0300

    Cria tela home  - Feita a criação da tela home, onde ao pesquisar, encaminharemos o usuário para a página de pesquisa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,45 @@
 <template>
-<div>
-  <musics-table :musics="['hheheehheh']"></musics-table>
-Musicas
-</div>
+  <v-row justify="center" style="height:100%" align="center">
+    <v-col cols="12" md="8" class="text-center">
+      <app-logo class="mb-5" center :width="isMobile ? '100%': '50%'"></app-logo>
+        <v-text-field clearable hide-details="auto" labgitel="Pesquisar" @keypress.enter="search(query)" @click:append="search(query)" v-model="query" :append-icon="icons.mdiMagnify" outlined rounded dense></v-text-field>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import MusicsTable from '@/components/MusicsTable.vue'
+import { computed, ref } from '@vue/composition-api'
+import AppLogo from '@core/components/app-logo/AppLogo.vue'
+import { mdiMagnify } from '@mdi/js'
+import { useRouter } from '@core/utils'
+import vuetify from '@/plugins/vuetify'
 
 export default {
   components: {
-    MusicsTable,
+    AppLogo,
+  },
+  setup() {
+    const isMobile = computed(() => vuetify.framework.breakpoint.mobile)
+    const query = ref('')
+    const { router } = useRouter()
+    const search = value => {
+      router.push({
+        name: 'search',
+        query: {
+          query: value,
+        },
+      })
+    }
+    const icons = {
+      mdiMagnify,
+    }
+
+    return {
+      isMobile,
+      query,
+      search,
+      icons,
+    }
   },
 }
 </script>
