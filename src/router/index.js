e7042d78cc839de2eb03b821a328161669ecc7f8
Yoharu <jonathas_a.a@hotmail.com>
2022-06-11 11:38:20 -0300

    Cria telas de autenticação

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
+import authRoutes from '@/router/auth.js'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,25 +13,28 @@ const routes = [
     name: 'home',
     component: () => import('@/views/Home.vue'),
     meta: {
-      layout: 'content',
+      search: false,
+    },
+  },
+  {
+    path: '/pesquisa',
+    name: 'search',
+    component: () => import('@/views/Search.vue'),
+    meta: {
+      search: false,
     },
   },
   {
     path: '/favoritos',
     name: 'favorite',
     component: () => import('@/views/Update.vue'),
-    meta: {
-      layout: 'content',
-    },
   },
   {
     path: '/atualizacoes',
     name: 'update',
     component: () => import('@/views/Favorite.vue'),
-    meta: {
-      layout: 'content',
-    },
   },
+  ...authRoutes,
   {
     path: '*',
     component: () => import('@/views/Error404.vue'),
