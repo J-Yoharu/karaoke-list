c9969b148fc9860d386383ed4b35f9cb2342d2cd
jonathas Augusto <jonathas_a.a@hotmail.com>
2022-07-08 14:00:19 -0300

    Altera lang do vuetify para pt

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,13 @@
 import preset from '@/@core/preset/preset'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
-
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  lang: {
+    locales: { pt: require('vuetify/lib/locale/pt').default },
+    current: 'pt',
+  },
   preset,
   icons: {
     iconfont: 'mdiSvg',
